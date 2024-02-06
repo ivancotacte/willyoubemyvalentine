084d6bd6d2db2e0d60f7a9778bfef8e5363bbbdb
Ivan Cotacte <cotactearmenion@gmail.com>
2024-02-06 23:50:17 +0800

    Natatae ako!

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const cros = require('cors');
 
 require('dotenv').config();
 require('ejs');
@@ -6,6 +9,18 @@ require('ejs');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+mongoose.connect(process.env.MONGO_URI, { dbName: "db_Ivan", useNewUrlParser: true, useUnifiedTopology: true });
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(cros());
+app.use(express.static('public'));
+app.set('view engine', 'ejs');
+
+
+app.get('/', (req, res) => {
+    res.render('index');
+});
 
 
 app.listen(PORT, () => {
