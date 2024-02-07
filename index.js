093f3d5e362516238cab6991c8cd1a3fef5520be
Ivan Cotacte <cotactearmenion@gmail.com>
2024-02-07 22:00:27 +0800

    Natatae ulit ako!

--- index.js
+++ index.js
@@ -22,6 +22,12 @@ app.get('/', (req, res) => {
     res.render('index');
 });
 
+app.get('/create', (req, res) => {
+    res.render('create');
+});
+
+
+
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
